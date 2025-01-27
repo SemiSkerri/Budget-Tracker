17b9c4226d2b850b08397551925c2696129dfb9d
Alex <alexhunter947@yahoo.com>
2025-01-26 20:15:40 -0500

    console log and changed the -= to just -

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Initialize budget values
 let totalBudget = 2500;
-let remainingBudget = totalBudget - expense;
+let remainingBudget = totalBudget;
 
 // Select elements
 const totalBudgetElement = document.getElementById('total-budget');
@@ -11,20 +11,26 @@ const addExpenseButton = document.getElementById('add-expense-btn');
 
 //Update the visual representation of the remaining budget
 function updateBudgetDisplay() {
+  console.log('Remaining Budget: ', remainingBudget);
   remainingBudgetElement.textContent = remainingBudget.toFixed(2);
   progressBar.value = remainingBudget;
 }
 
 //Handle expense submission
 addExpenseButton.addEventListener('click', () => {
+  console.log('Button clicked');
+  
   const expense = parseFloat(expenseInput.value);
+  console.log('Entered Expense: ', expense);
 
   if(!isNAN(expense) && expense > 0 && expense <= remainingBudget) {
-    remainingBudget -= expense;
+    remainingBudget - expense;
+    console.log('New Remaining Budget: ', remainingBudget);
     updateBudgetDisplay();
     expenseInput.value = ''; //reset input field
   }
    else {
+     console.log('Invalid expense');
      alert('Please enter a valid expense amount or ensure it doesn\'t exceed remaining budget.');
    }
 });
